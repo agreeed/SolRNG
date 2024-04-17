90b9687572395c8a01e5547524ca603c15dae8a6
abstraact <108076105+agreeed@users.noreply.github.com>
2024-04-17 15:05:19 +0500

    Update farmer2.lua

--- farmer2.lua
+++ farmer2.lua
@@ -6,12 +6,17 @@ game:GetService("RunService").Heartbeat:Connect(function()
   for i, v in pairs(workspace:WaitForChild("DroppedItems"):GetDescendants()) do
     if v:FindFirstChildOfClass("ProximityPrompt") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
       local prox = v:FindFirstChildOfClass("ProximityPrompt") 
-      prox.RequiresLineOfSight = false
-      prox.MaxActivationDistance = 10000
-      prox:InputHoldBegin()
-      task.wait()
-      prox:InputHoldEnd()
-      task.wait()
+
+      if fireproximityprompt then
+            fireproximityprompt(prox, 2.48)
+      else
+            prox.RequiresLineOfSight = false
+            prox.MaxActivationDistance = 10000
+            prox:InputHoldBegin()
+            task.wait()
+            prox:InputHoldEnd()
+            task.wait()
+      end
     end
   end
 end)
