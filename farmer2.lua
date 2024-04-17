dd309ba13e8713edd376fbbab6ed3030a29c84ec
abstraact <108076105+agreeed@users.noreply.github.com>
2024-04-17 14:55:56 +0500

    Tried removing movement

--- farmer2.lua
+++ farmer2.lua
@@ -5,7 +5,7 @@ vhrp.Anchored = true
 game:GetService("RunService").Heartbeat:Connect(function()
   for i, v in pairs(workspace:WaitForChild("DroppedItems"):GetDescendants()) do
     if v:FindFirstChildOfClass("ProximityPrompt") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-      local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+      --[[local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
       tws:Create(vhrp, TweenInfo.new(1), {["CFrame"] = CFrame.new(v.Position + Vector3.new(math.random(-5, 5), 3, math.random(-5, 5)))}):Play()
       task.spawn(function()
         for i = 1, 60 do
@@ -14,7 +14,8 @@ game:GetService("RunService").Heartbeat:Connect(function()
         end
       end)
       local prox = v:FindFirstChildOfClass("ProximityPrompt")
-
+      ]]
+                        
       prox.RequiresLineOfSight = false
       prox.MaxActivationDistance = 10000
       prox:InputHoldBegin()
