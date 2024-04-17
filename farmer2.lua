b6b10e202343e1ae6c0d8cbddef2dfb1b8884d6e
abstraact <108076105+agreeed@users.noreply.github.com>
2024-04-17 14:44:45 +0500

    Update farmer2.lua

--- farmer2.lua
+++ farmer2.lua
@@ -1,7 +1,7 @@
 game:GetService("RunService").Heartbeat:Connect(function()
   for i, v in pairs(workspace:WaitForChild("DroppedItems"):GetDescendants()) do
     if v:FindFirstChildOfClass("ProximityPrompt") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
-      game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position)
+      game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position + Vector3.new(math.random(-5, 5), 3, math.random(-5, 5)))
       local prox = v:FindFirstChildOfClass("ProximityPrompt")
 
       prox.RequiresLineOfSight = false
