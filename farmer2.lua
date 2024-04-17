6f94cd36b08ffef9163918aeeaa3ecf28c872794
abstraact <108076105+agreeed@users.noreply.github.com>
2024-04-17 14:52:53 +0500

    Replace movement with VHRP

--- farmer2.lua
+++ farmer2.lua
@@ -1,11 +1,18 @@
 local tws = game:GetService("TweenService")
-
+local vhrp = Instance.new("Part")
+vhrp.Anchored = true
+      
 game:GetService("RunService").Heartbeat:Connect(function()
   for i, v in pairs(workspace:WaitForChild("DroppedItems"):GetDescendants()) do
     if v:FindFirstChildOfClass("ProximityPrompt") and game:GetService("Players").LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
       local hrp = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
-      tws:Create(hrp, TweenInfo.new(1), {["CFrame"] = CFrame.new(v.Position + Vector3.new(math.random(-5, 5), 3, math.random(-5, 5)))}):Play()
-      task.wait(1)
+      tws:Create(vhrp, TweenInfo.new(1), {["CFrame"] = CFrame.new(v.Position + Vector3.new(math.random(-5, 5), 3, math.random(-5, 5)))}):Play()
+      task.spawn(function()
+        for i = 1, 60 do
+          task.wait()
+          hrp.CFrame = vhrp.CFrame
+        end
+      end)
       local prox = v:FindFirstChildOfClass("ProximityPrompt")
 
       prox.RequiresLineOfSight = false
