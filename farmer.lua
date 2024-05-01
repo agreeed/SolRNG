e26ae18a0ba900e628ebb2986036d2abe52be16a
abstraact <108076105+agreeed@users.noreply.github.com>
2024-05-01 20:36:06 +0500

    Update farmer.lua

--- farmer.lua
+++ farmer.lua
@@ -134,9 +134,9 @@ local success, err = pcall(function()
 			if bl then
 				local s, e = pcall(function()
 					talk(bl)
-					slap(p, "DefaultSlap", Vector3.yAxis * 0)
-					root.AssemblyLinearVelocity = Vector3.yAxis * 500
-					root.AssemblyAngularVelocity = Vector3.new(math.random(), math.random(), math.random()) * 1000
+					slap(p, "DefaultSlap", Vector3.xAxis * 2)
+					root.AssemblyLinearVelocity = Vector3.yAxis * 1
+					root.AssemblyAngularVelocity = Vector3.new(math.random(), math.random(), math.random()) * 10
 					root.CanCollide = false
 				end)
 
